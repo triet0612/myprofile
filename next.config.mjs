7aafe9d12f58dc8e54ead68f500f5c6f37b7ecaf
trietdang <triet0612@gmail.com>
2024-09-09 22:30:13 +0700

    small

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,23 @@
 /** @type {import('next').NextConfig} */
 
-import remarkGfm from 'remark-gfm'
+import remarkGfm from "remark-gfm";
 import createMDX from "@next/mdx";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: "export",
   // Configure `pageExtensions` to include markdown and MDX files
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
   // Optionally, add any other Next.js config below
   images: {
-    loader: 'custom',
-    loaderFile: './my-loader.ts',
+    loader: "custom",
+    loaderFile: "./my-loader.ts",
   },
 };
 
 const withMDX = createMDX({
   // Add markdown plugins here, as desired
-  options: {
-    remarkPlugins: [remarkGfm],
-    rehypePlugins: [],
-  },
+  options: {},
 });
 
 // Merge MDX config with Next.js config
